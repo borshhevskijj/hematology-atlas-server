d4432b2eea099bd82d404c4e276650ba1eae7c17
lemonsqueezy <borshhevskijj98@mail.ru>
2023-07-31 15:47:43 +0300

    test v5

--- controller/controller.js
+++ controller/controller.js
@@ -1,7 +1,7 @@
 import BloodCellModel from "../model/model.js";
-import nodeCache from "node-cache";
+// import nodeCache from "node-cache";
 const bloodCellModel = new BloodCellModel();
-const cache = new nodeCache();
+// const cache = new nodeCache();
 
 export const firstCharToUpperCase = (word) => {
   const lowerCaseWord = word.toLowerCase();
@@ -11,7 +11,6 @@ export const firstCharToUpperCase = (word) => {
 const filterData = (data) => {
   console.log(data);
   const [images, description] = data;
-  // const sortedDescription = description.sort((a,b)=>a.id - b.id)
   const results = [];
   description.map((bloodCell) => {
     const img = images.filter((image) => image.bloodCell_id === bloodCell.id).map((image) => image.image);
@@ -58,22 +57,6 @@ export const getBloodCellsDescriptionByNameHandler = (request) => {
   });
 };
 
-// const getImagesByHemopoiesisHandler = (request) => {
-//   return new Promise((resolve, reject) => {
-//     bloodCellModel.getImagesByHemopoiesis(request.params.type, (err, rows) => {
-//       if (err) {
-//         reject(err);
-//       } else if (rows && rows.length > 0) {
-//         resolve(rows);
-//       } else {
-//         reject({
-//           error: "cell not found",
-//         });
-//       }
-//     });
-//   });
-// };
-
 const getImagesHandler = (modelMethod, requestParams) => {
   console.log("qwe");
   return new Promise((resolve, reject) => {
@@ -93,17 +76,17 @@ const getImagesHandler = (modelMethod, requestParams) => {
 
 export const getBloodCellsByHematopoiesisHandler = async (request, response) => {
   try {
-    const cachedResult = cache.get(request.params.type);
-    if (cachedResult) {
-      response.status(200).json(cachedResult);
-      return;
-    }
+    // const cachedResult = cache.get(request.params.type);
+    // if (cachedResult) {
+    //   response.status(200).json(cachedResult);
+    //   return;
+    // }
     const images = await getImagesHandler(bloodCellModel.getImagesByHemopoiesis, request.params.type);
     const bloodCellDescription = await getBloodCellsDescriptionByHemopoiesisHandler(request);
 
     const result = filterData([images, bloodCellDescription]);
     response.status(200).json(result);
-    cache.set(request.params.type, result, 3600);
+    // cache.set(request.params.type, result, 3600);
   } catch (error) {
     response.status(500).json({ error: error.message });
   }
@@ -114,17 +97,17 @@ export const getBloodCellsByNameHandler = async (request, response) => {
   console.log("qwe");
   const requestPramsName = firstCharToUpperCase(request.params.name);
   try {
-    const cachedResult = cache.get(requestPramsName);
-    if (cachedResult) {
-      response.status(200).json(cachedResult);
-      return;
-    }
+    // const cachedResult = cache.get(requestPramsName);
+    // if (cachedResult) {
+    //   response.status(200).json(cachedResult);
+    //   return;
+    // }
     const images = await getImagesHandler(bloodCellModel.getImagesByName, requestPramsName);
     const bloodCellDescription = await getBloodCellsDescriptionByNameHandler(request);
     const result = filterData([images, bloodCellDescription]);
 
     response.status(200).json(...result);
-    cache.set(requestPramsName, ...result, 3600);
+    // cache.set(requestPramsName, ...result, 3600);
   } catch (error) {
     response.status(404).json({ error: "клетка не найдена!" });
   }
--- index.js
+++ index.js
@@ -22,4 +22,20 @@ app.get("/allCells", (request, response) => {
   getAllBloodCellsNamesHandler(request, response);
 });
 
+// -----
+
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
 app.listen(PORT, () => console.log(`SERVER STARTED ON ${PORT} PORT`));
+
+module.exports = app;
