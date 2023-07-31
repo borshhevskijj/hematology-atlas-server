03c590d99f94f7a6fe8f6de5730e0bec0c9d9500
lemonsqueezy <borshhevskijj98@mail.ru>
2023-07-31 17:30:14 +0300

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 import express from "express";
 import cors from "cors";
-// import {
-//   getBloodCellsByHematopoiesisHandler,
-//   getBloodCellsByNameHandler,
-//   getAllBloodCellsNamesHandler,
-// } from "./controller/controller.js";
+import {
+  getBloodCellsByHematopoiesisHandler,
+  getBloodCellsByNameHandler,
+  getAllBloodCellsNamesHandler,
+} from "./controller/controller.js";
 
 const app = express();
 const PORT = 5000;
 app.use(express.json(), cors({ origin: "*" }));
 
-// app.get("/hematopoiesis/:type", (request, response) => {
-//   getBloodCellsByHematopoiesisHandler(request, response);
-// });
-
-app.get("/search/:name", (request, response) => {
-  // getBloodCellsByNameHandler(request, response);
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+app.get("/hematopoiesis/:type", (request, response) => {
+  getBloodCellsByHematopoiesisHandler(request, response);
 });
 
-// app.get("/allCells", (request, response) => {
-// getAllBloodCellsNamesHandler(request, response);
-// });
+app.get("/search/:name", (request, response) => {
+  getBloodCellsByNameHandler(request, response);
+});
+
+app.get("/allCells", (request, response) => {
+  getAllBloodCellsNamesHandler(request, response);
+});
 
 app.listen(PORT, () => console.log(`SERVER STARTED ON ${PORT} PORT`));
