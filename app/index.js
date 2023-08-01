1ebeff2b3f01202ea88b15cbf23d7d4eb6efa2e8
lemonsqueezy <borshhevskijj98@mail.ru>
2023-08-01 17:05:32 +0300

    test v10

--- app/index.js
+++ app/index.js
@@ -12,28 +12,16 @@ app.use(express.static('public'))
 
 
 app.get("/hematopoiesis/:type", (request, response) => {
-  // getBloodCellsByHematopoiesisHandler(request, response);
-  response.setHeader('Content-Type', 'text/html');
-  response.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  response.end(`Item: hello hematopoiesis/:type`);
-  console.log('1');
+  getBloodCellsByHematopoiesisHandler(request, response);
 });
 
 app.get("/search/:name", (request, response) => {
-  // getBloodCellsByNameHandler(request, response);
-  response.setHeader('Content-Type', 'text/html');
-  response.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  response.end(`Item: hello /search/:name`);
-  console.log('2');
+  getBloodCellsByNameHandler(request, response);
 });
 
 app.get("/allCells", (request, response) => {
-  response.setHeader('Content-Type', 'text/html');
-  response.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  response.end(`Item: hello /allCells`);
-  console.log('3');
-  // getAllBloodCellsNamesHandler(request, response);
+  getAllBloodCellsNamesHandler(request, response);
 });
 
-// export default app;
-module.exports = app
+export default app;
+// module.exports = app
