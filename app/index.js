ccb90ba635976f250c2baf09f3b63f3cb47d12d0
lemonsqueezy <borshhevskijj98@mail.ru>
2023-08-02 13:41:54 +0300

    test v15

--- app/index.js
+++ app/index.js
@@ -1,27 +1,28 @@
 import express from "express";
 import cors from "cors";
-import {
-  getBloodCellsByHematopoiesisHandler,
-  getBloodCellsByNameHandler,
-  getAllBloodCellsNamesHandler,
-} from "../controller/controller.js";
+// import {
+//   getBloodCellsByHematopoiesisHandler,
+//   getBloodCellsByNameHandler,
+//   getAllBloodCellsNamesHandler,
+// } from "../controller/controller.js";
 
 const app = express();
-app.use(express.json(), cors({ origin: "*" }));
-app.use(express.static('public'))
 
+app.use(express.json(), cors({ origin: "*" }));
 
 app.get("/hematopoiesis/:type", (request, response) => {
-  getBloodCellsByHematopoiesisHandler(request, response);
+  // getBloodCellsByHematopoiesisHandler(request, response);
+  res.send(`working!!! ${req.params.type}`);
 });
 
 app.get("/search/:name", (request, response) => {
-  getBloodCellsByNameHandler(request, response);
+  // getBloodCellsByNameHandler(request, response);
+  res.send(`working!!! ${req.params.name}`);
 });
 
 app.get("/allCells", (request, response) => {
   getAllBloodCellsNamesHandler(request, response);
+  // res.send(`working!!!`);
 });
 
 export default app;
-// module.exports = app
