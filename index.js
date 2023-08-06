d2fbfb16806f004a72d54d3440e71edfc99d6a1c
lemonsqueezy <borshhevskijj98@mail.ru>
2023-08-06 15:54:51 +0300

    test v10

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 // const PORT = 4000;
 app.use(express.json(), cors({ origin: "*" }));
 
-app.get("/hematopoiesis", (request, response) => {
+app.get("/", (request, response) => {
   // getBloodCellsByHematopoiesisHandler(request, response);
   response.send("Express on Vercel");
 });
