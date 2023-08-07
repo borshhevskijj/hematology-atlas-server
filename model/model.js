1d16312820f87ba0232180bccd66860ea8dcfb90
lemonsqueezy <borshhevskijj98@mail.ru>
2023-08-07 17:47:59 +0300

    Finalized v1.1

--- model/model.js
+++ model/model.js
@@ -1,9 +1,13 @@
 import fetch from "node-fetch";
+import { env } from "node:process";
 import "dotenv/config";
 
 export default class BloodCellModel {
   async getBloodCellDescriptionByName(name) {
-    const url = `https://api.tinybird.co/v0/pipes/getBloodCellsByName.json?token=${process.env.getBloodCellDescriptionByNameToken}&name=${name}`;
+    const url = `https://api.tinybird.co/v0/pipes/getBloodCellsByName.json?token=${env.getBloodCellDescriptionByNameToken}&name=${name}`;
+    console.log(`env.getBloodCellDescriptionByNameToken- ${env.getBloodCellDescriptionByNameToken}`);
+    console.log(`process.env.getBloodCellDescriptionByNameToken- ${process.env.getBloodCellDescriptionByNameToken}`);
+    console.log(name, "name");
     try {
       const response = await fetch(url);
       if (!response.ok) {
@@ -26,7 +30,12 @@ export default class BloodCellModel {
   }
 
   async getBloodCellsDescriptionByHemopoiesis(name) {
-    const url = `https://api.tinybird.co/v0/pipes/getBloodCellsByHematopoiesis.json?token=${process.env.getBloodCellsDescriptionByHemopoiesisToken}&name=${name}`;
+    const url = `https://api.tinybird.co/v0/pipes/getBloodCellsByHematopoiesis.json?token=${env.getBloodCellsDescriptionByHemopoiesisToken}&name=${name}`;
+    console.log(`env.getBloodCellsDescriptionByHemopoiesisToken- ${env.getBloodCellsDescriptionByHemopoiesisToken}`);
+    console.log(
+      `process.env.getBloodCellsDescriptionByHemopoiesisToken- ${process.env.getBloodCellsDescriptionByHemopoiesisToken}`
+    );
+    console.log(name, "name");
     try {
       const response = await fetch(url);
       if (!response.ok) {
@@ -49,7 +58,10 @@ export default class BloodCellModel {
   }
 
   async getImagesByHemopoiesis(name) {
-    const url = `https://api.tinybird.co/v0/pipes/getImagesByHematopoiesis.json?token=${process.env.getImagesByHemopoiesisToken}&name=${name}`;
+    const url = `https://api.tinybird.co/v0/pipes/getImagesByHematopoiesis.json?token=${env.getImagesByHemopoiesisToken}&name=${name}`;
+    console.log(`env.getImagesByHemopoiesisToken- ${env.getImagesByHemopoiesisToken}`);
+    console.log(`process.env.getImagesByHemopoiesisToken- ${process.env.getImagesByHemopoiesisToken}`);
+    console.log(name, "name");
     try {
       const response = await fetch(url);
       if (!response.ok) {
@@ -73,7 +85,10 @@ export default class BloodCellModel {
   }
 
   async getImagesByName(name) {
-    const url = `https://api.tinybird.co/v0/pipes/getImagesByName.json?token=${process.env.getImagesByNameToken}&name=${name}`;
+    const url = `https://api.tinybird.co/v0/pipes/getImagesByName.json?token=${env.getImagesByNameToken}&name=${name}`;
+    console.log(`env.getImagesByNameToken- ${env.getImagesByNameToken}`);
+    console.log(`process.env.getImagesByNameToken- ${process.env.getImagesByNameToken}`);
+    console.log(name, "name");
 
     try {
       const response = await fetch(url);
@@ -97,8 +112,9 @@ export default class BloodCellModel {
   }
 
   async getAllBloodCellsNames() {
-    const url = `https://api.tinybird.co/v0/pipes/getAllBloodCellsNames.json?token=${process.env.getAllBloodCellsNamesToken}`;
-
+    const url = `https://api.tinybird.co/v0/pipes/getAllBloodCellsNames.json?token=${env.getAllBloodCellsNamesToken}`;
+    console.log(`env.getBloodCellDescriptionByNameToken- ${env.getAllBloodCellsNamesToken}`);
+    console.log(`process.env.getBloodCellDescriptionByNameToken- ${process.env.getAllBloodCellsNamesToken}`);
     try {
       const response = await fetch(url);
       if (!response.ok) {
